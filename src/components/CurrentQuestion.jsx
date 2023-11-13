5af22440ee6fb3901bfd5bb857d223541925da1f
psterner <psterner@hotmail.com>
2023-11-13 17:24:42 +0000

    Update store with questionId and answerId

--- src/components/CurrentQuestion.jsx
+++ src/components/CurrentQuestion.jsx
@@ -1,6 +1,9 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { quiz } from "../reducers/quiz";
 
 export const CurrentQuestion = () => {
+  const dispatch = useDispatch();
+  // Getting the current question from the store
   const question = useSelector(
     (state) => state.quiz.questions[state.quiz.currentQuestionIndex]
   );
@@ -9,14 +12,31 @@ export const CurrentQuestion = () => {
     return <h1>Oh no! I could not find the current question!</h1>;
   }
 
-  const handleClick = (event) => {
-    // TODO send the captureSquare action
+  /**
+   * returns a payload with questionId and answerIndex
+   */
+  const handleSubmitAnswer = (answerIndex) => {
+    // Use the quiz action to handle the submit answer
+    dispatch(
+      quiz.actions.submitAnswer({
+        questionId: question.id,
+        answerIndex,
+      })
+    );
   };
 
   return (
     <>
       <h2>Current question: {question.id}</h2>
       <p>{question.questionText}</p>
+
+      <ul>
+        {question.options.map((option, index) => (
+          <li key={index}>
+            <button onClick={() => handleSubmitAnswer(index)}>{option}</button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
