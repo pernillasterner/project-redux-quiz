2a5571971a607657cd2fbb4cdea69df1c4e81892
psterner <psterner@hotmail.com>
2023-11-14 17:04:37 +0000

    Move questions to seperate file

---
+++ src/data/questions.jsx
@@ -0,0 +1,50 @@
+export const questions = [
+  {
+    id: 1,
+    questionText:
+      "What does the term 'minimalism' in interior design refer to?",
+    options: [
+      "Maximal use of colors",
+      "Excessive ornamentation",
+      "Simple and uncluttered spaces",
+      "Eclectic design styles",
+    ],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: 2,
+    questionText: "Which material is commonly used for creating accent walls?",
+    options: ["Fabric", "Concrete", "Wallpaper", "Metal"],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: 3,
+    questionText: "What is the purpose of an area rug in interior design?",
+    options: [
+      "To add warmth and comfort",
+      "To cover imperfections in the floor",
+      "To reflect natural light",
+      "To create a modern look",
+    ],
+    correctAnswerIndex: 0,
+  },
+  {
+    id: 4,
+    questionText:
+      "Which color scheme uses colors that are next to each other on the color wheel?",
+    options: ["Complementary", "Monochromatic", "Analogous", "Triadic"],
+    correctAnswerIndex: 2,
+  },
+  {
+    id: 5,
+    questionText:
+      "What does the term 'feng shui' relate to in interior design?",
+    options: [
+      "Use of bold colors",
+      "Spatial arrangement for harmony",
+      "Vintage and antique furniture",
+      "Geometric patterns",
+    ],
+    correctAnswerIndex: 1,
+  },
+];
