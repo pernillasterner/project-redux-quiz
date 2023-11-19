afd319ebb8293c650c7c16d3d83ba49a0a9042c7
psterner <psterner@hotmail.com>
2023-11-19 18:19:29 +0100

    Move Summery component & add class to change j-content

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import { CurrentQuestion } from "./components/Quiz/QuizBody/CurrentQuestion/CurrentQuestion";
 import { Welcome } from "./components/Welcome/Welcome";
+import { Summery } from "./components/Summery/Summery";
 
 export const App = () => {
   const [quizStarted, setQuizStarted] = useState(false);
+  const quizOver = useSelector((state) => state.quiz.quizOver);
 
   return (
-    <div className="quizContainer">
-      {quizStarted ? (
-        <CurrentQuestion />
-      ) : (
-        <Welcome onStartQuiz={() => setQuizStarted(true)} />
-      )}
+    <div className={`quizContainer ${!quizStarted || quizOver ? "end" : ""}`}>
+      {!quizStarted && <Welcome onStartQuiz={() => setQuizStarted(true)} />}
+      {quizStarted && !quizOver && <CurrentQuestion />}
+      {quizOver && <Summery />}
     </div>
   );
 };
