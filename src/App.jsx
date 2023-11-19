7170a2422f4387b73152a91c7e39982b09ee065d
psterner <psterner@hotmail.com>
2023-11-19 12:11:36 +0100

    Update path to currentQuestion

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from "react";
-import { CurrentQuestion } from "./components/Quiz/CurrentQuestion/CurrentQuestion";
+import { CurrentQuestion } from "./components/Quiz/QuizBody/CurrentQuestion/CurrentQuestion";
 import { Welcome } from "./components/Welcome/Welcome";
 
 export const App = () => {
   const [quizStarted, setQuizStarted] = useState(false);
 
   return (
-    <>
+    <div className="quizContainer">
       {quizStarted ? (
         <CurrentQuestion />
       ) : (
         <Welcome onStartQuiz={() => setQuizStarted(true)} />
       )}
-    </>
+    </div>
   );
 };
