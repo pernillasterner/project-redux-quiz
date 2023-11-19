ac1119a47b91f0248f9faa569c4519e82164bd5d
psterner <psterner@hotmail.com>
2023-11-19 12:22:09 +0100

    Add Welome message. Set basic styling

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,21 @@
+import "./Welcome.scss";
+
+export const Welcome = ({ onStartQuiz }) => {
+  return (
+    <div className="welcomeContainer">
+      <div className="logoContainer">
+        {" "}
+        <span className="logo">Q</span>
+      </div>
+      <h3>Multi-step Quiz</h3>
+      <h1>Welcome to Quiz</h1>
+      <p className="instructions">
+        Test your knowledge with our quiz. <br />
+        Are you ready?
+      </p>
+      <button className="submit" onClick={onStartQuiz}>
+        START QUIZ
+      </button>
+    </div>
+  );
+};
