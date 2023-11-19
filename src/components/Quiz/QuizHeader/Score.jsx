fbfb7fb265bb67c94b07f375bfdc1c3217fe62eb
psterner <psterner@hotmail.com>
2023-11-19 17:21:46 +0100

    Make score global and set logic in score component

--- src/components/Quiz/QuizHeader/Score.jsx
+++ src/components/Quiz/QuizHeader/Score.jsx
@@ -1,27 +1,20 @@
-import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { quiz } from "../../../reducers/quiz";
 
 export const Score = () => {
-  const [score, setScore] = useState(0);
-
+  const dispatch = useDispatch();
+  const score = useSelector((state) => state.quiz.score);
   const answer = useSelector(
     (state) => state.quiz.answers[state.quiz.currentQuestionIndex]
   );
 
   useEffect(() => {
     if (answer) {
-      switch (answer.isCorrect) {
-        case true:
-          setScore(score + 10);
-          break;
-        case false:
-          setScore(score - 5);
-          break;
-        default:
-          break;
-      }
+      // Update the score in the store
+      dispatch(quiz.actions.updateScore(answer.isCorrect ? 10 : -5));
     }
-  }, [answer]);
+  }, [answer, dispatch]);
 
   return (
     <div className="score">
