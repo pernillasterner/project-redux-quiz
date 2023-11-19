81aaa8fa951c65a4cb9fc03fb7afa9b7aab368a3
psterner <psterner@hotmail.com>
2023-11-19 18:21:02 +0100

    Add squares to welcome page

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,20 +2,24 @@ import "./Welcome.scss";
 
 export const Welcome = ({ onStartQuiz }) => {
   return (
-    <div className="welcomeContainer">
-      <div className="logoContainer">
-        {" "}
-        <span className="logo">Q</span>
+    <>
+      <div className="squareLeft"></div>
+      <div className="squareRight"></div>
+      <div className="welcomeContainer">
+        <div className="logoContainer">
+          {" "}
+          <span className="logo">Q</span>
+        </div>
+        <h3>Multi-step Quiz</h3>
+        <h1>Welcome 😃</h1>
+        <p className="instructions">
+          Test your knowledge with our quiz. <br />
+          Are you ready?
+        </p>
+        <button className="submit" onClick={onStartQuiz}>
+          START QUIZ
+        </button>
       </div>
-      <h3>Multi-step Quiz</h3>
-      <h1>Welcome to Quiz</h1>
-      <p className="instructions">
-        Test your knowledge with our quiz. <br />
-        Are you ready?
-      </p>
-      <button className="submit" onClick={onStartQuiz}>
-        START QUIZ
-      </button>
-    </div>
+    </>
   );
 };
