e2e1d31a8c7fac2fb4481fd8e45ebb1c73dbde34
psterner <psterner@hotmail.com>
2023-11-21 16:18:19 +0100

    Add timer in the condition when updating score

--- src/components/Quiz/QuizHeader/Score.jsx
+++ src/components/Quiz/QuizHeader/Score.jsx
@@ -8,9 +8,10 @@ export const Score = () => {
   const answer = useSelector(
     (state) => state.quiz.answers[state.quiz.currentQuestionIndex]
   );
+  const timer = useSelector((state) => state.quiz.timer);
 
   useEffect(() => {
-    if (answer) {
+    if (answer && timer !== 0) {
       // Update the score in the store
       dispatch(quiz.actions.updateScore(answer.isCorrect ? 10 : -5));
     }
