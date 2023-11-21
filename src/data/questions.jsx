1246ae51b1e575bfe1d0792ee487096eb3492752
psterner <psterner@hotmail.com>
2023-11-21 16:15:29 +0100

    Add image to quiz

--- src/data/questions.jsx
+++ src/data/questions.jsx
@@ -1,6 +1,13 @@
 export const questions = [
   {
     id: 1,
+    questionText: "Is Philippe Starck a renowned French designer?",
+    options: ["Yes", "No"],
+    image: "/src/assets/images/R1304N_STARCK.jpg",
+    correctAnswerIndex: 0,
+  },
+  {
+    id: 2,
     questionText:
       "What does the term 'minimalism' in interior design refer to?",
     options: [
@@ -12,13 +19,13 @@ export const questions = [
     correctAnswerIndex: 2,
   },
   {
-    id: 2,
+    id: 3,
     questionText: "Which material is commonly used for creating accent walls?",
     options: ["Fabric", "Concrete", "Wallpaper", "Metal"],
     correctAnswerIndex: 2,
   },
   {
-    id: 3,
+    id: 4,
     questionText: "What is the purpose of an area rug in interior design?",
     options: [
       "To add warmth and comfort",
@@ -29,14 +36,14 @@ export const questions = [
     correctAnswerIndex: 0,
   },
   {
-    id: 4,
+    id: 5,
     questionText:
       "Which color scheme uses colors that are next to each other on the color wheel?",
     options: ["Complementary", "Monochromatic", "Analogous", "Triadic"],
     correctAnswerIndex: 2,
   },
   {
-    id: 5,
+    id: 6,
     questionText:
       "What does the term 'feng shui' relate to in interior design?",
     options: [
